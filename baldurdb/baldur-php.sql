e9f30c980195a6bceaa950652af4ae311f03cf9d
IvanByFrost <ivanwander99@gmail.com>
2025-05-08 21:34:47 -0500

    baldurcomplete

--- baldurdb/baldur-php.sql
+++ baldurdb/baldur-php.sql
@@ -106,6 +106,12 @@ ON parent_student (parent_id);
 CREATE INDEX student_index_1
 ON parent_student (student_id);
 
+--Tabla de días de la semana.
+CREATE TABLE week_day (
+	week_id INT PRIMARY KEY AUTO_INCREMENT,
+	name_day VARCHAR(20) NOT NULL,
+);
+
 --Tabla de los horarios de clases.
 CREATE TABLE schedule_school (
 	schedule_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -115,42 +121,43 @@ CREATE TABLE schedule_school (
 	end_time TIME NOT NULL
 );
 
+--Tabla de los docentes. 
 CREATE TABLE professor (
 	professor_id INT PRIMARY KEY AUTO_INCREMENT,
 	user_main_id INT NOT NULL,
     FOREIGN KEY (user_main_id) REFERENCES user_main(user_main_id)
 );
 
+--Tabla de los grupos de clases.
 CREATE TABLE class_group (
 	class_group_id INT PRIMARY KEY AUTO_INCREMENT,
     class_group_name varchar(6) NOT NULL,
     group_quota TINYINT NOT NULL,
-	class_group_professor_id INT NOT NULL, 
+	professor_id INT NOT NULL, 
     FOREIGN KEY (professor_id) REFERENCES professor(professor_id)
+    grade_id INT NOT NULL,
+    FOREIGN KEY (grade_id) REFERENCES grade(grade_id)
 );
 
+--Tabla de los grados. 
 CREATE TABLE grade (
 	grade_id INT PRIMARY KEY AUTO_INCREMENT,
 	name_grade VARCHAR(10) NOT NULL,
 	level_grade VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE group_grade (
-	group_grade_id INT PRIMARY KEY AUTO_INCREMENT,
-	class_group_id INT NOT NULL, 
-    FOREIGN KEY (class_group_id) REFERENCES class_group(class_group_id),
-	grade_id INT NOT NULL,
-    FOREIGN KEY (grade_id) REFERENCES grade(grade_id) 
-);
-
+--Tabla puente entre estudiantes y grupos de clases.
 CREATE TABLE student_group (
 	student_group_id INT PRIMARY KEY AUTO_INCREMENT,
 	class_group_id INT NOT NULL,
     FOREIGN KEY (class_group_id) REFERENCES class_group(class_group_id),
 	student_id INT NOT NULL,
-    FOREIGN KEY (student_id) REFERENCES student(student_id)
+    FOREIGN KEY (student_id) REFERENCES student(student_id),
+    school_year YEAR NOT NULL,
 );
 
+--Tabla puente entre grupos de clases y horarios.
+-- Esta tabla permite que un grupo de clases tenga varios horarios.
 CREATE TABLE group_schedule (
 	group_schedule_id INT PRIMARY KEY AUTO_INCREMENT,
 	class_group_id INT NOT NULL,
@@ -158,7 +165,7 @@ CREATE TABLE group_schedule (
 	schedule_id INT NOT NULL,
     FOREIGN KEY (schedule_id) REFERENCES schedule_school(schedule_id)
 );
-
+--Tabla de los períodos académicos.
 CREATE TABLE academic_term (
 	academic_term_id INT PRIMARY KEY AUTO_INCREMENT,
 	academic_term_name VARCHAR(20) NOT NULL,
@@ -166,6 +173,8 @@ CREATE TABLE academic_term (
 	end_date_term DATE NOT NULL
 );
 
+--Tabla puente entre horarios y períodos académicos.
+-- Esta tabla permite que un horario tenga varios períodos académicos.
 CREATE TABLE schedule_academic_term (
 	schedule_academic_term_id INT PRIMARY KEY AUTO_INCREMENT,
 	schedule_id INT not null,
@@ -174,11 +183,14 @@ CREATE TABLE schedule_academic_term (
     FOREIGN KEY (academic_term_id) REFERENCES academic_term(academic_term_id)
 );
 
+--Tabla de las asignaturas.
 CREATE TABLE subject_school (
 	subject_id INT PRIMARY KEY AUTO_INCREMENT,
 	name_subject VARCHAR(50)
 );
 
+--Tabla puente entre asignaturas y horarios.
+-- Esta tabla permite que una asignatura tenga varios horarios.
 CREATE TABLE subject_schedule (
 	subject_schedule_id INT PRIMARY KEY AUTO_INCREMENT,
 	schedule_id INT NOT NULL,
@@ -187,6 +199,8 @@ CREATE TABLE subject_schedule (
     FOREIGN KEY (subject_id) REFERENCES subject_school(subject_id)
 );
 
+--Tabla de los temas de las asignaturas.
+-- Esta tabla permite que una asignatura tenga varios temas.
 CREATE TABLE topic (
 	topic_id INT PRIMARY KEY AUTO_INCREMENT,
 	topic_name VARCHAR(30) NOT NULL,
@@ -194,14 +208,7 @@ CREATE TABLE topic (
     FOREIGN KEY (subject_id) REFERENCES subject_school(subject_id)
 );
 
-CREATE TABLE activity (
-	activity_id INT PRIMARY KEY AUTO_INCREMENT,
-	kind_activity VARCHAR(10) NOT NULL,
-	topic_id INT NOT NULL,
-    FOREIGN KEY (topic_id) REFERENCES topic(topic_id),
-	score_activity DECIMAL(10,2)
-);
-
+--Tabla de roles con turnos. 
 CREATE TABLE shift_role (
         shift_role INT PRIMARY KEY AUTO_INCREMENT,
         shift_user_id INT not null, 
@@ -210,6 +217,7 @@ CREATE TABLE shift_role (
         FOREIGN KEY (role_duty_user_id) REFERENCES role_user(role_duty_user_id)
 );
 
+--Tabla de rectores. 
 CREATE TABLE headmaster (
 	headmaster_id INT PRIMARY KEY AUTO_INCREMENT,
 	user_main_id INT not null,
@@ -217,18 +225,21 @@ CREATE TABLE headmaster (
 	office_hm VARCHAR(10) NOT NULL
 );
 
+--Tabla de coordinadores.
 CREATE TABLE coordinator (
 	coordinator_id INT PRIMARY KEY AUTO_INCREMENT,
 	user_main_id INT not null,
     FOREIGN KEY (user_main_id) REFERENCES user_main(user_main_id)
 );
 
+--Tabla de contadores. 
 CREATE TABLE treasurer (
 	treasurer_id INT PRIMARY KEY AUTO_INCREMENT,
 	user_main_id INT not null,
     FOREIGN KEY (user_main_id) REFERENCES user_main(user_main_id)
 );
 
+--Tabla de los colegios.
 CREATE TABLE school (
 	school_id INT PRIMARY KEY AUTO_INCREMENT,
 	school_name VARCHAR(60) NOT NULL,
@@ -237,6 +248,8 @@ CREATE TABLE school (
     FOREIGN KEY (headmaster_id) REFERENCES headmaster(headmaster_id)
 );
 
+--Tabla de los campus de los colegios.
+-- Esta tabla permite que un colegio tenga varios campus.
 CREATE TABLE school_campus (
 	school_campus_id INT PRIMARY KEY AUTO_INCREMENT,
 	school_id INT not null,
@@ -247,6 +260,8 @@ CREATE TABLE school_campus (
 	status_campus BIT NOT NULL
 );
 
+--Tabla puente entre los colegios y los grados.
+-- Esta tabla permite que un colegio tenga varios grados.
 CREATE TABLE school_grade (
 	school_grade_id INT PRIMARY KEY AUTO_INCREMENT,
 	school_campus_id INT not null,
@@ -255,11 +270,14 @@ CREATE TABLE school_grade (
     FOREIGN KEY REFERENCES grade(grade_id)
 );
 
+--Tabla de salones.
 CREATE TABLE classroom (
 	classroom_id INT PRIMARY KEY AUTO_INCREMENT,
 	classroom_name VARCHAR(10) NOT NULL
 );
 
+--Tabla puente entre los colegios y los salones.
+-- Esta tabla permite que un colegio tenga varios salones.
 CREATE TABLE school_classroom (
 	school_classroom_id INT PRIMARY KEY AUTO_INCREMENT,
 	school_campus_id INT not null,
@@ -268,6 +286,7 @@ CREATE TABLE school_classroom (
     FOREIGN KEY (classroom_id) REFERENCES classroom(classroom_id)
 );
 
+--Tabla de eventos escolares.
 CREATE TABLE event_school (
 	event_id INT PRIMARY KEY AUTO_INCREMENT,
 	type_event VARCHAR(60) NOT NULL,
@@ -275,6 +294,8 @@ CREATE TABLE event_school (
 	end_date DATETIME NOT NULL
 );
 
+--Tabla puente entre los colegios y los eventos.
+-- Esta tabla permite que un colegio tenga varios eventos.
 CREATE TABLE school_campus_event (
 	school_campus_event_id INT PRIMARY KEY AUTO_INCREMENT,
 	school_campus_id INT not null,
@@ -283,11 +304,13 @@ CREATE TABLE school_campus_event (
     FOREIGN KEY (event_id) REFERENCES event_school(event_id)
 );
 
+--Tabla de bancos.
 CREATE TABLE bank (
 	bank_id INT PRIMARY KEY AUTO_INCREMENT,
 	name_bank varchar(20)
 );
 
+--Tabla de cuentas bancarias.
 CREATE TABLE account (
 	account_id INT PRIMARY KEY AUTO_INCREMENT,
 	account_number VARCHAR(34) NOT NULL,
@@ -297,6 +320,8 @@ CREATE TABLE account (
     FOREIGN KEY (treasurer_id) REFERENCES treasurer(treasurer_id) 
 );
 
+--Tabla puente entre los colegios y las cuentas bancarias.
+-- Esta tabla permite que un colegio tenga varias cuentas bancarias.
 CREATE TABLE school_account (
 	school_account_id INT PRIMARY KEY AUTO_INCREMENT,
 	account_id INT not null,
@@ -305,7 +330,7 @@ CREATE TABLE school_account (
     FOREIGN KEY (school_id) REFERENCES school(school_id)
 );
 
-
+--Tabla de transacciones bancarias.
 CREATE TABLE transaction_user (
 	transaction_id INT PRIMARY KEY AUTO_INCREMENT,
 	transaction_number VARCHAR(50) NOT NULL,
@@ -315,6 +340,8 @@ CREATE TABLE transaction_user (
 	concept_transaction VARCHAR(40)
 );
 
+--Tabla puente entre las cuentas bancarias y las transacciones.
+-- Esta tabla permite que una cuenta bancaria tenga varias transacciones.
 CREATE TABLE account_transaction (
 	account_transaction INT PRIMARY KEY AUTO_INCREMENT,
 	account_id INT NOT NULL,
@@ -325,6 +352,8 @@ CREATE TABLE account_transaction (
     FOREIGN KEY (student_account_id) REFERENCES student_account(student_account_id)
 );
 
+--Tabla de calificaciones.
+-- Esta tabla permite que un estudiante tenga varias calificaciones.
 CREATE TABLE score (
 	score_id INT PRIMARY KEY AUTO_INCREMENT,
 	student_id INT NOT NULL,
@@ -333,9 +362,9 @@ CREATE TABLE score (
     FOREIGN KEY REFERENCES academic_term(academic_term_id),
 	activity_id INT not null,
     FOREIGN KEY (activity_id) REFERENCES activity(activity_id),
-	score_value DECIMAL(10,2)
 );
 
+--Tabla de asistencias.
 CREATE TABLE attendance (
 	attendance_id INT PRIMARY KEY AUTO_INCREMENT,
 	check_in_at DATETIME  NOT NULL,
@@ -351,6 +380,7 @@ CREATE TABLE attendance (
     FOREIGN KEY REFERENCES professor(professor_id)
 );
 
+--Tabla de observadores.
 CREATE TABLE conduct_report (
 	conduct_report_id INT PRIMARY KEY AUTO_INCREMENT,
 	student_id INT NOT NULL,
@@ -361,6 +391,8 @@ CREATE TABLE conduct_report (
 	register_date datetime NOT NULL
 );
 
+--Tabla de reportes de conducta.
+-- Esta tabla permite que un estudiante tenga varios reportes de conducta.
 CREATE TABLE summon (
 	summon_id INT PRIMARY KEY AUTO_INCREMENT,
 	professor_id INT NOT NULL,
@@ -374,6 +406,7 @@ CREATE TABLE summon (
 	summon_date datetime NOT NULL
 );
 
+--Tabla puente entre los observadores y los reportes de conducta.
 CREATE TABLE conduct_report_summon (
 	conduct_report_summon_id INT PRIMARY KEY AUTO_INCREMENT,
 	conduct_report_id INT NOT NULL, 
@@ -382,12 +415,14 @@ CREATE TABLE conduct_report_summon (
     FOREIGN KEY (summon_id) REFERENCES summon(summon_id) 
 );
 
+--Tabla de chats.
 CREATE TABLE chat (
 	chat_id INT PRIMARY KEY AUTO_INCREMENT,
 	name_chat varchar(20),
 	chat_type varchar(10)
 );
 
+--Tabla de mensajes.
 CREATE TABLE message_user (
 	message_id INT PRIMARY KEY AUTO_INCREMENT,
 	content_message TEXT,
@@ -395,6 +430,8 @@ CREATE TABLE message_user (
 	sent_date datetime
 );
 
+--Tabla de notificaciones.
+-- Esta tabla permite que un usuario tenga varias notificaciones.
 CREATE TABLE notification_chat (
 	notification_id INT PRIMARY KEY AUTO_INCREMENT,
 	user_main_id INT NOT NULL,
@@ -402,6 +439,9 @@ CREATE TABLE notification_chat (
 	content_message TEXT
 );
 
+--Tabla puente entre los chats y los usuarios.
+-- Esta tabla permite que un chat tenga varios usuarios.
+-- Esta tabla permite que un usuario tenga varios chats.
 CREATE TABLE chat_user (
 	chat_user_id INT PRIMARY KEY AUTO_INCREMENT,
 	chat_id INT not null,
@@ -410,6 +450,8 @@ CREATE TABLE chat_user (
     FOREIGN KEY (user_main_id)REFERENCES user_main(user_main_id)
 );
 
+--Tabla puente entre los mensajes y los chats.
+-- Esta tabla permite que un chat tenga varios mensajes.
 CREATE TABLE chat_message_id (
 	chat_message_id INT PRIMARY KEY AUTO_INCREMENT,
 	chat_id INT not null,
@@ -418,13 +460,16 @@ CREATE TABLE chat_message_id (
     FOREIGN KEY (message_id) REFERENCES message_user(message_id)
 );
 
+--Tabla de tipos de actividades.
 CREATE TABLE activity_type (
-    activity_type_id INT IDENTITY(1,1) PRIMARY KEY,
+    activity_type_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL
 );
 
+--Tabla de actividades.
 CREATE TABLE activity (
-    activity_id INT IDENTITY(1,1) PRIMARY KEY,
+    activity_id INT PRIMARY KEY AUTO_INCREMENT,
+	activity_name VARCHAR(50) NOT NULL,
     activity_type_id INT NOT NULL,
     topic_id INT NOT NULL,
     score_activity DECIMAL(10,2),
@@ -433,19 +478,19 @@ CREATE TABLE activity (
     FOREIGN KEY (topic_id) REFERENCES topic(topic_id)
 );
 
+--Tabla de actividades de tipo sopa de letras.
 CREATE TABLE activity_word (
-    word_id INT IDENTITY(1,1) PRIMARY KEY,
+    word_id INT PRIMARY KEY AUTO_INCREMENT,
     activity_id INT NOT NULL,
     word VARCHAR(50) NOT NULL,
-
     FOREIGN KEY (activity_id) REFERENCES activity(activity_id)
 );
 
+--Tabla de actividades de tipo crucigrama.
 CREATE TABLE activity_grid (
-    grid_id INT IDENTITY(1,1) PRIMARY KEY,
+    grid_id INT PRIMARY KEY AUTO_INCREMENT,
     activity_id INT NOT NULL,
     grid_text TEXT NOT NULL,
     grid_size INT NOT NULL,
-
     FOREIGN KEY (activity_id) REFERENCES activity(activity_id)
 );
