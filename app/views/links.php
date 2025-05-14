2e95388e0b65c44e722c7ffbb5a75c76da62bbea
IvanByFrost <ivanwander99@gmail.com>
2025-05-13 19:37:55 -0500

    Refactor stylesheets: remove unused mainstyle.css; consolidate and update CSS links in header and contact pages; add new styles for features, contact, and slider sections for improved organization and maintainability.

--- app/views/links.php
+++ app/views/links.php
@@ -1,4 +1,8 @@
-    <link rel="stylesheet" href="<?= BASE_URL ?>/app/views/assets/css/mainstyle.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/app/views/assets/css/home.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/app/views/assets/css/header.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/app/views/assets/css/features.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/app/views/assets/css/contact.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/app/views/assets/css/slider.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/app/views/assets/css/footer.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/app/views/assets/css/loginstyle.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/app/views/assets/css/privacystyle.css">
@@ -8,5 +12,5 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0">
 
-    <link rel="icon" type="image/png" href="<?= BASE_URL ?>/app/views/assets/img/favicon.png">
+    <link rel="icon" type="image/png" href="<?= BASE_URL ?>/app\views\assets\img\favicon.png">
 </head>
