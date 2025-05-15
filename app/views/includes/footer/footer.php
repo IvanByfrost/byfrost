158f7e6d425dcf12f34643f15542b7f3eca13474
IvanByFrost <ivanwander99@gmail.com>
2025-05-15 05:31:36 -0500

    Refactor footer and links: update contact and site map sections for improved structure and accessibility; add Font Awesome for icon support.

--- app/views/includes/footer/footer.php
+++ app/views/includes/footer/footer.php
@@ -1,30 +1,34 @@
 <footer>
-        <div class="container">
-            <div class="footer-content">
-                <div class="about">
-                    <h2>Nosotros</h2>
-                    <p>Nuestra solución tecnológica innovadora propone transformar la gestión administrativa de las instituciones educativas, ofreciendo una plataforma intuitiva y sostenible.</p>
-                </div>
-        <div class="contact">
-                    <h2>Contacto</h2>
-                    <p>Cra 7 # 98-25, Bogotá, Colombia</p>
-                    <p>(601) 7886590</p>
-                    <p>(601) 4567890</p>
-                    <a href="www.byfrost.com.co">www.byfrost.com.co</a>
-                    <p>info@byfrost.com</p>
-                </div>
-        <div class="site-map">
-                    <p><a href="#">Inicio</a></p>
-                    <p><a href="plans.htm">Planes</a></p>
-                    <p><a href="contact.htm">Contáctenos</a></p>
-                    <p><a href="/byfrost/pages/privacy/guide.php">FAQ</a></p>
-                    <p><a href="site-map.htm">Mapa del sitio</a></p>
-                </div>
+    <div class="container">
+        <div class="footer-content">
+            <div class="about">
+                <h2>Nosotros</h2>
+                <p>Nuestra solución tecnológica innovadora propone transformar la gestión administrativa de las instituciones educativas, ofreciendo una plataforma intuitiva y sostenible.</p>
             </div>
-    <div class = "copyright">
+            <div class="contact">
+                <h2>Contacto</h2>
+                <ul class="contact-info">
+                    <li><i class="fas fa-map-marker-alt"></i> <a href="https://maps.app.goo.gl/jqFvKv6PwTjzrKBF7"> Cra 7 # 98-25, Bogotá, Colombia</a></li>
+                    <li><i class="fas fa-phone"></i> <a href="tel:+576017886590"> Tel: (601) 7886590</a></li>
+                    <li><i class="fas fa-phone"></i> <a href="tel:+573135678788"> Cel: (+57) 3135678788</a></li>
+                    <li><i class="fas fa-globe"></i> <a href="https://www.byfrost.com.co" target="_blank">www.byfrost.com.co</a></li>
+                    <li><i class="fas fa-envelope"></i> <a href="mailto:info@byfrost.com">info@byfrost.com</a></li>
+                </ul>
+            </div>
+            <div class="site-map">
+                <ul class="site-map">
+                    <li><a href="#"><i class="fas fa-home"></i> Inicio</a></li>
+                    <li><a href="plans.htm"><i class="fas fa-tags"></i> Planes</a></li>
+                    <li><a href="contact.htm"><i class="fas fa-phone"></i> Contáctenos</a></li>
+                    <li><a href="\byfrost\app\views\privacy\guide.php"><i class="fas fa-question-circle"></i> FAQ</a></li>
+                    <li><a href="site-map.htm"><i class="fas fa-sitemap"></i> Mapa del sitio</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="copyright">
             <p>Byfrost &copy; 2026. Todos los derechos reservados.</p>
             <p>Diseñado por Byfrost Software.</p>
         </div>
     </div>
-    </footer>
+</footer>
 </body>
