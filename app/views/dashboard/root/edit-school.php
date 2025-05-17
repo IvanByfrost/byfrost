1ad92037b1cb1316e1d8a56c85517f18fa234c60
IvanByFrost <ivanwander99@gmail.com>
2025-05-16 20:59:34 -0500

    Refactor dashboard forms: enhance layout and styling for school registration and editing; add search functionality and improve button designs.

---
+++ app/views/dashboard/root/edit-school.php
@@ -0,0 +1,38 @@
+<?php
+include __DIR__ . '/menu-dashboard.php';
+?>
+<div class="main-dashboard">
+    <div class="mdashboard-header">
+        <h1>Bienvenido al Dashboard del Colegio</h1>
+        <p>Desde aquí puedes gestionar los colegios de Byfrost.</p>
+    </div>
+
+    <div class="main-dashboard">
+        <form class="dash-form" id="school-form" method="POST">
+            <div class="form-header">
+                <h2><i data-lucide="school"></i>Editar colegio</h2>
+                <p>Aquí puedes editar los datos del colegio.</p>
+            </div>
+            <div class="search-container">
+                <form class="search-container" action="#" method="GET">
+                    <input type="text" placeholder="Buscar..." name="q">
+                    <button type="submit">🔍</button>
+            </div>
+            <div class="input-grid">
+                <input type="text" id="school-name" placeholder="Nombre del colegio" required>
+                <input type="text" id="school-adress" placeholder="Dirección del colegio" required>
+                <input type="number" id="school-phone" placeholder="Teléfono del colegio" required>
+                <input type="text" id="school-email" placeholder="Email del colegio" required>
+                <input type="text" id="school-website" placeholder="Página web del colegio" required>
+                <input type="number" id="headmaster" placeholder="Documento del rector" required>
+            </div>
+            <div class="form-footer">
+                <button class="btn-register" type="submit" id="submit-school">Actualizar</button>
+                <button class="btn-cancel" type="button" id="cancel-school">Cancelar</button>
+            </div>
+        </form>
+    </div>
+    <?php
+    include __DIR__ . '/../../includes/footer/footer.php';
+    include __DIR__ . '/../../includes/root-scripts.php';
+    ?>
--- app/views/dashboard/root/school.php
+++ app/views/dashboard/root/school.php
@@ -1,29 +1,27 @@
 <?php
 include __DIR__ . '/menu-dashboard.php';
 ?>
+
 <div class="main-dashboard">
     <div class="mdashboard-header">
         <h1>Bienvenido al Dashboard del Colegio</h1>
         <p>Desde aquí puedes gestionar los colegios de Byfrost.</p>
     </div>
-    <form class="dash-form" id="school-form" method="POST">
-        <div class="form-header">
-            <h2><i data-lucide="school"></i>Registrar colegio</h2>
-            <p>Rellena los siguientes campos con los datos del colegio</p>
+
+    <div class="dashboard-cards">
+                <div class="card">
+            <h3>Crear un colegio</h3>
+            <p>Registra los colegios aquí</p>
+            <a href="create-school.php" class="btn btn-info">Crear</a>
         </div>
-        <div class="input-grid">
-            <input type="text" id="school-name" placeholder="Nombre del colegio" required>
-            <input type="text" id="school-adress" placeholder="Dirección del colegio" required>
-            <input type="text" id="school-phone" placeholder="Teléfono del colegio" required>
-            <input type="text" id="school-email" placeholder="Email del colegio" required>
-            <input type="text" id="school-website" placeholder="Página web del colegio" required>
-            <input type="text" id="headmaster" placeholder="Documento del rector" required>
+                <div class="card">
+            <h3>Editar un colegio</h3>
+            <p>Edita la información los colegios aquí</p>
+            <a href="edit-school.php" class="btn btn-info">Editar</a>
         </div>
-        <div class="form-footer">
-            <button class="btn-register" type="submit" id="submit-school">Registrar colegio</button>
-            <button class="btn-cancel" type="button" id="cancel-school">Cancelar</button>
-        </div>
-    </form>
+    </div>
+
+
     <?php
     include __DIR__ . '/../../includes/footer/footer.php';
     include __DIR__ . '/../../includes/root-scripts.php';
